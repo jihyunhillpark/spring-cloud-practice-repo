8bfae2b11f7e2bf71361f9c138a16fa5cfda2fa7
jihyunhillpark <girl9505@naver.com>
2023-08-06 00:38:22 +0900

    section2 : API Gateway Service - gateway를 통한 라우팅

---
+++ api-gateway-service/spring-cloud-routing/src/test/java/com/example/springcloudrouting/SpringCloudRoutingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudrouting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudRoutingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
