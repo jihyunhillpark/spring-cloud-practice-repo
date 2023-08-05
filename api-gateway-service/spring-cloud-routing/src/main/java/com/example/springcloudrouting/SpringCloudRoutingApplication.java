bce24db2cc19721b375a1e2d4982958d9c451acf
jihyunhillpark <girl9505@naver.com>
2023-08-06 01:56:59 +0900

    section2 : API Gateway Service - 필터추가하기

--- api-gateway-service/spring-cloud-routing/src/main/java/com/example/springcloudrouting/SpringCloudRoutingApplication.java
+++ api-gateway-service/spring-cloud-routing/src/main/java/com/example/springcloudrouting/SpringCloudRoutingApplication.java
@@ -2,29 +2,12 @@ package com.example.springcloudrouting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringCloudRoutingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudRoutingApplication.class, args);
-	}
-
-	@Bean
-	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-		return builder.routes()
-				.route(p -> p
-						.path("/first-service/**")
-						.filters(f -> f.rewritePath("/first-service/(?<segment>.*)", "/${segment}"))
-						.uri("http://localhost:8001"))
-				.route(p -> p
-						.path("/second-service/**")
-						.filters(f -> f.rewritePath("/second-service/(?<segment>.*)", "/${segment}"))
-						.uri("http://localhost:8002"))
-				.build();
 	}
 
 }
