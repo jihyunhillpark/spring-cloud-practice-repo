88de8fe4e05ec344ec0f4d222269da630043507d
jihyunhillpark <girl9505@naver.com>
2023-08-06 02:10:01 +0900

    section2 : API Gateway Service - java코드에서 application설정 방식으로 바꿔보기

--- api-gateway-service/spring-cloud-routing/src/main/java/com/example/springcloudrouting/config/FilterConfig.java
+++ api-gateway-service/spring-cloud-routing/src/main/java/com/example/springcloudrouting/config/FilterConfig.java
@@ -5,21 +5,21 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class FilterConfig {
 
-    @Bean
-    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route(r-> r.path("/first-service/**")
-                        .filters(f -> f.addRequestHeader("first-request","first-request-header")
-                                .addResponseHeader("first-response","first-response-header"))
-                        .uri("http://localhost:8001")) // filter적용해서 uri로 이동
-                .route(r-> r.path("/second-service/**")
-                        .filters(f -> f.addRequestHeader("second-request","second-request-header")
-                                .addResponseHeader("second-response","second-response-header"))
-                        .uri("http://localhost:8002"))
-                .build();
-    }
+//    @Bean
+//    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route(r-> r.path("/first-service/**")
+//                        .filters(f -> f.addRequestHeader("first-request","first-request-header")
+//                                .addResponseHeader("first-response","first-response-header"))
+//                        .uri("http://localhost:8001")) // filter적용해서 uri로 이동
+//                .route(r-> r.path("/second-service/**")
+//                        .filters(f -> f.addRequestHeader("second-request","second-request-header")
+//                                .addResponseHeader("second-response","second-response-header"))
+//                        .uri("http://localhost:8002"))
+//                .build();
+//    }
 
 }
