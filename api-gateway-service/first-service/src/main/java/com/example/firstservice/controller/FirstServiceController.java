fd0b433cf4382f98109845467fb89d2111d1879d
jihyunhillpark <girl9505@naver.com>
2023-08-06 14:52:13 +0900

    section2 : API Gateway Service - 멀티 인스턴스 환경에 대한 로드밸런싱 확인 작업 추가(only first serive)

--- api-gateway-service/first-service/src/main/java/com/example/firstservice/controller/FirstServiceController.java
+++ api-gateway-service/first-service/src/main/java/com/example/firstservice/controller/FirstServiceController.java
@@ -1,15 +1,26 @@
 package com.example.firstservice.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
 @RestController
 @RequestMapping("first-service")
 public class FirstServiceController {
+    Environment env; // 환경정보 설정을 가져오는 전통적인 방법
+
+    @Autowired
+    public FirstServiceController(Environment env) { //spring 컨텍스트에 빈생성될 때 env객체를 같이 가져옴
+        this.env = env;
+    }
+
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the First Service";
@@ -22,7 +33,9 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, there. This is a message form FirstService";
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}", request.getServerPort());
+        return String.format("Hi, there. This is a message form FirstService on PORT %s",
+                env.getProperty("local.server.port"));
     }
 }
