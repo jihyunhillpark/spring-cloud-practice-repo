7ebfefb32872f2715fb2c3190275653e01dea91f
jihyunhillpark <girl9505@naver.com>
2023-08-06 11:44:25 +0900

    section2 : API Gateway Service - eureka도 함께 사용하여 요청 분산하기

--- service-discovery/discovery-service/src/main/java/com/example/discoveryservice/DiscoveryServiceApplication.java
+++ service-discovery/discovery-service/src/main/java/com/example/discoveryservice/DiscoveryServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.springcloudpractice;
+package com.example.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class SpringCloudPracticeApplication {
+public class DiscoveryServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringCloudPracticeApplication.class, args);
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
     }
 
 }
--- service-discovery/discovery-service/src/test/java/com/example/discoveryservice/SpringCloudPracticeApplicationTests.java
+++ service-discovery/discovery-service/src/test/java/com/example/discoveryservice/SpringCloudPracticeApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.springcloudpractice;
+package com.example.discoveryservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringCloudPracticeApplicationTests {
+class DiscoveryServiceApplicationTests {
 
     @Test
     void contextLoads() {
