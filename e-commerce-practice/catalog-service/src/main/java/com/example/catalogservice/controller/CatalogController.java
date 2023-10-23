4e68743e2ec3f5974863b7f84dd4043f7fdd7934
jihyunhillpark <girl9505@naver.com>
2023-10-24 02:09:37 +0900

    section5-3 카탈로그 서비스 재로드.

---
+++ e-commerce-practice/catalog-service/src/main/java/com/example/catalogservice/controller/CatalogController.java
@@ -0,0 +1,37 @@
+package com.example.catalogservice.controller;
+
+import com.example.catalogservice.jpa.CatalogEntity;
+import com.example.catalogservice.service.CatalogService;
+import com.example.catalogservice.vo.ResponseCatalog;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/catalog-service")
+public class CatalogController {
+    private final Environment env;
+    private final CatalogService catalogService;
+
+    @GetMapping("/health_check")
+    public String status() {
+        return String.format("It's Working in User Service PORT %s", env.getProperty("local.server.port"));
+    }
+    @GetMapping("/catalogs")
+    public ResponseEntity<List<ResponseCatalog>> getCatalogs() {
+        Iterable<CatalogEntity> catalogs = catalogService.getAllCatalogs();
+        List<ResponseCatalog> result = new ArrayList<>();
+        ModelMapper modelMapper = new ModelMapper();
+        catalogs.forEach(v -> result.add(modelMapper.map(v, ResponseCatalog.class)));
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+}
---
+++ e-commerce-practice/catalog-service/src/main/java/com/example/catalogservice/vo/ResponseCatalog.java
@@ -0,0 +1,18 @@
+package com.example.catalogservice.vo;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ResponseCatalog {
+    private String productId;
+    private String productName;
+    private Integer unitPrice;
+    private Integer stock;
+    private LocalDateTime createAt;
+}
