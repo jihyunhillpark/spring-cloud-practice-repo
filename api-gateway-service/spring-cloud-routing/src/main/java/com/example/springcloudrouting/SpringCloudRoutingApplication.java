59556f0af46c85432e79174efabead9769f9f4f0
jihyunhillpark <girl9505@naver.com>
2023-11-06 01:17:36 +0900

    section7. API gateway 서비스에 spring actuator 적용하기.

--- api-gateway-service/spring-cloud-routing/src/main/java/com/example/springcloudrouting/SpringCloudRoutingApplication.java
+++ api-gateway-service/spring-cloud-routing/src/main/java/com/example/springcloudrouting/SpringCloudRoutingApplication.java
@@ -1,7 +1,11 @@
 package com.example.springcloudrouting;
 
+import com.sun.net.httpserver.HttpExchange;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringCloudRoutingApplication {
@@ -10,4 +14,8 @@ public class SpringCloudRoutingApplication {
 		SpringApplication.run(SpringCloudRoutingApplication.class, args);
 	}
 
+	@Bean
+	public HttpTraceRepository httpTraceRepository( ){
+		return new InMemoryHttpTraceRepository();
+	}
 }
